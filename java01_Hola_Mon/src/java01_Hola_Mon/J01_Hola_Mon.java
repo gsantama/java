8ec6b88eb49c95f5b21a0c19882d30e2141ca177
gsantama <gregorio.santamaria@gmail.com>
2018-09-06 00:08:56 +0200

    V01

---
+++ java01_Hola_Mon/src/java01_Hola_Mon/J01_Hola_Mon.java
@@ -0,0 +1,10 @@
+package java01_Hola_Mon;
+
+public class J01_Hola_Mon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mon");
+	}
+
+}
